f1dcc3a993fdfe47841c1bc240e58ac1d7c5e362
holmfridur <holmfridurh17@ru.is>
2020-08-17 14:52:03 +0000

    fb

--- facebookTemplate.php
+++ facebookTemplate.php
@@ -1,3 +1,33 @@
 <?php
-$facebookLink = 0;
+    function wpb_hook_javascript_header_fb() {
+        ?>
+            <script>
+			function inject_facebook(link) {
+				var menuItems = document.getElementsByClassName('parent-menu-item');
+				var i = menuItems.length - 1;
+				var prevElem = menuItems[i];
+				var container = document.createElement('li');
+				container.classList.add('menu-item');
+				var fbIcon = document.createElement('a');
+				fbIcon.href = link;
+				fbIcon.rel = "nofollow";
+				fbIcon.target = "_blank";
+				fbIcon.classList.add('facebook-link');
+				var span = document.createElement('span');
+				span.classList.add('ar-icon-facebook');
+				fbIcon.append( span );
+				container.append( fbIcon );
+				prevElem.parentNode.insertBefore(container, prevElem.nextSibling);
+			}
+			var link = document.getElementById('fb').dataset.link;
+			console.log(link);
+			if (link) {
+				inject_facebook(link);
+			}
+        
+    
+        </script>
+    <?php
+}
+add_action('wp_footer', 'wpb_hook_javascript_header_fb');
 ?>
--- functions.php
+++ functions.php
@@ -32,7 +32,6 @@ add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 /*gitprufa*/
 function wpb_hook_javascript_header() {
 	?>
-	<span id="fb" data-link=$facebookLink></span>
 		<script>
 
 			function change_search_icon() {
@@ -45,29 +44,6 @@ function wpb_hook_javascript_header() {
 				}
 			}
 			//change_search_icon();
-			
-			function inject_facebook(link) {
-				var menuItems = document.getElementsByClassName('parent-menu-item');
-				var i = menuItems.length - 1;
-				var prevElem = menuItems[i];
-				var container = document.createElement('li');
-				container.classList.add('menu-item');
-				var fbIcon = document.createElement('a');
-				fbIcon.href = link;
-				fbIcon.rel = "nofollow";
-				fbIcon.target = "_blank";
-				fbIcon.classList.add('facebook-link');
-				var span = document.createElement('span');
-				span.classList.add('ar-icon-facebook');
-				fbIcon.append( span );
-				container.append( fbIcon );
-				prevElem.parentNode.insertBefore(container, prevElem.nextSibling);
-			}
-			var link = document.getElementById('fb').dataset.link;
-			console.log(link);
-			if (link) {
-				inject_facebook(link);
-			}
 			
 			
 			var nodes = document.getElementsByClassName('icon');
