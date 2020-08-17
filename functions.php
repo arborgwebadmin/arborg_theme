a5ed64aa4e06769f09850bc0f8d9d9fe7444c88c
holmfridur <holmfridurh17@ru.is>
2020-08-17 14:01:56 +0000

    fb

--- functions.php
+++ functions.php
@@ -61,7 +61,7 @@ function wpb_hook_javascript_header() {
 				container.append( fbIcon );
 				prevElem.parentNode.insertBefore(container, prevElem.nextSibling);
 			}
-			var link = <?php echo json_encode($facebookLink, JSON_HEX_TAG); ?>;
+			var link = <?php echo $facebookLink; ?>;
 			console.log(link);
 			if (link) {
 				inject_facebook(link);
